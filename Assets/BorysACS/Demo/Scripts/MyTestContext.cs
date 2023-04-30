bd306659ff00b0f8b9a93ab2b4646a22d099438f
Roman Boryslavskyy <romanborys02@gmail.com>
2023-04-30 20:45:23 +0200

    [ADD] Started adding some test assets

---
+++ Assets/BorysACS/Demo/Scripts/MyTestContext.cs
@@ -0,0 +1,11 @@
+using BorysACS.Core;
+using UnityEngine;
+
+namespace BorysACS.Demo.Scripts
+{
+    [CreateAssetMenu(fileName = "Context.Test.asset", menuName = "BorysACS/Demo/New Test Context", order = 0)]
+    public class MyTestContext : Context
+    {
+        
+    }
+}
---
+++ Assets/BorysACS/Demo/Scripts/MyTestWorld.cs
@@ -0,0 +1,10 @@
+using BorysACS.Core;
+using UnityEngine;
+
+namespace BorysACS.Demo.Scripts
+{
+    [CreateAssetMenu(fileName = "World.Test.asset", menuName = "BorysACS/Demo/New Test World", order = 1)]
+    public class MyTestWorld : World
+    {
+    }
+}
